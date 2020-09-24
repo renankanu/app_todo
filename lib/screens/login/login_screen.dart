8ad68bf9974d3beff0b27d5d0de8abdfe36a2c7e
renankanu <renankanu@gmail.com>
2020-09-24 00:09:32 -0300

    Feature - implements login with firebase

--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -1,3 +1,4 @@
+import 'package:app_todo/services/auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -14,11 +15,75 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text("Login"),
+        child: Padding(
+          padding: const EdgeInsets.all(60.0),
+          child: Builder(builder: (BuildContext context) {
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextFormField(
+                  textAlign: TextAlign.center,
+                  decoration: const InputDecoration(hintText: "Username"),
+                  controller: _emailController,
+                ),
+                TextFormField(
+                  textAlign: TextAlign.center,
+                  decoration: const InputDecoration(hintText: "Password"),
+                  controller: _passwordController,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                RaisedButton(
+                  onPressed: () async {
+                    final String returnValue =
+                        await Auth(auth: widget.auth).signIn(
+                      email: _emailController.text,
+                      password: _passwordController.text,
+                    );
+                    if (returnValue == "Success") {
+                      _emailController.clear();
+                      _passwordController.clear();
+                    } else {
+                      Scaffold.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text(returnValue),
+                        ),
+                      );
+                    }
+                  },
+                  child: const Text("Sign In"),
+                ),
+                FlatButton(
+                  onPressed: () async {
+                    final String returnValue =
+                        await Auth(auth: widget.auth).createAccount(
+                      email: _emailController.text,
+                      password: _passwordController.text,
+                    );
+                    if (returnValue == "Success") {
+                      _emailController.clear();
+                      _passwordController.clear();
+                    } else {
+                      Scaffold.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text(returnValue),
+                        ),
+                      );
+                    }
+                  },
+                  child: const Text("Create Account"),
+                )
+              ],
+            );
+          }),
+        ),
       ),
     );
   }
