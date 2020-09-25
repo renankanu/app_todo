1df09bcb1048905783b49105de23fd9a1db26de7
renankanu <renankanu@gmail.com>
2020-09-24 23:12:31 -0300

    Create database fire store

--- lib/screens/core/core_screen.dart
+++ lib/screens/core/core_screen.dart
@@ -1,3 +1,7 @@
+import 'package:app_todo/models/todo.dart';
+import 'package:app_todo/services/auth.dart';
+import 'package:app_todo/services/database.dart';
+import 'package:app_todo/widgets/todo_card.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
@@ -13,11 +17,106 @@ class CoreScreen extends StatefulWidget {
 }
 
 class _CoreScreenState extends State<CoreScreen> {
+  final TextEditingController _todoController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Core Screen'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Kanu Todo"),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            key: const ValueKey("signOut"),
+            icon: const Icon(Icons.exit_to_app),
+            onPressed: () {
+              Auth(auth: widget.auth).signOut();
+            },
+          )
+        ],
+      ),
+      body: Column(
+        children: <Widget>[
+          const SizedBox(
+            height: 20,
+          ),
+          const Text(
+            "Add Todo Here:",
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Card(
+            margin: const EdgeInsets.all(20),
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      key: const ValueKey("addField"),
+                      controller: _todoController,
+                    ),
+                  ),
+                  IconButton(
+                    key: const ValueKey("addButton"),
+                    icon: const Icon(Icons.add),
+                    onPressed: () {
+                      if (_todoController.text != "") {
+                        setState(() {
+                          Database(firestore: widget.firestore).addTodo(
+                              uid: widget.auth.currentUser.uid,
+                              content: _todoController.text);
+                          _todoController.clear();
+                        });
+                      }
+                    },
+                  )
+                ],
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          const Text(
+            "Your Todos",
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Expanded(
+            child: StreamBuilder(
+              stream: Database(firestore: widget.firestore)
+                  .streamTodos(uid: widget.auth.currentUser.uid),
+              builder: (BuildContext context,
+                  AsyncSnapshot<List<TodoModel>> snapshot) {
+                if (snapshot.connectionState == ConnectionState.active) {
+                  if (snapshot.data.isEmpty) {
+                    return const Center(
+                      child: Text("You don't have any unfinished Todos"),
+                    );
+                  }
+                  return ListView.builder(
+                    itemCount: snapshot.data.length,
+                    itemBuilder: (_, index) {
+                      return TodoCard(
+                        firestore: widget.firestore,
+                        uid: widget.auth.currentUser.uid,
+                        todo: snapshot.data[index],
+                      );
+                    },
+                  );
+                } else {
+                  return const Center(
+                    child: Text("loading..."),
+                  );
+                }
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
