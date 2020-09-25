6adda716f2059fc4aa4ed855c069ea8d49178796
renankanu <renankanu@gmail.com>
2020-09-24 21:48:50 -0300

    Feature - add icons and images

---
+++ lib/screens/welcome/welcome_screen.dart
@@ -0,0 +1,61 @@
+import 'package:app_todo/screens/login/login_screen.dart';
+import 'package:app_todo/screens/welcome/widgets/background.dart';
+import 'package:app_todo/widgets/rounded_button.dart';
+import 'package:flutter/material.dart';
+
+import '../../constants.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Background(
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              "WELCOME TO EDU",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: size.height * 0.05),
+            SvgPicture.asset(
+              "assets/icons/chat.svg",
+              height: size.height * 0.45,
+            ),
+            SizedBox(height: size.height * 0.05),
+            RoundedButton(
+              text: "LOGIN",
+              press: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return LoginScreen();
+                    },
+                  ),
+                );
+              },
+            ),
+            RoundedButton(
+              text: "SIGN UP",
+              color: kPrimaryLightColor,
+              textColor: Colors.black,
+              press: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return SignUpScreen();
+                    },
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
